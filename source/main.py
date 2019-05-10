8a5a470fa5753e0ab5e9509eff8e2477c6644d1a
Juan Ferrand <JuanFerrand@users.noreply.github.com>
2019-05-10 20:48:55 -0300

    Uploaded files

---
+++ source/main.py
@@ -0,0 +1,13 @@
+import image_diff
+
+
+def main():
+
+    image_diff.printDifference()
+    image_diff.showImage()
+    image_diff.showImageGray()
+    image_diff.printImageThresh()
+    image_diff.printImageContours()
+
+if __name__ == "__main__":
+    main()
